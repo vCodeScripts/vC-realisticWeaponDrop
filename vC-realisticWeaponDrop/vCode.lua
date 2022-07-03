2126c73c0737e24f828cca23e0cf46110336f4d4
Poyraz <97455492+poypoydev@users.noreply.github.com>
2022-07-03 13:00:32 +0200

    Update vCode.lua

--- vC-realisticWeaponDrop/vCode.lua
+++ vC-realisticWeaponDrop/vCode.lua
@@ -1,7 +1,7 @@
 vCode = {} -- 'whitelist is the other type, makes use of the vCode.WhitelistedWeapons table'
 
 vCode.BlacklistedWeapons = {}
-
+vCode.Key = 74
 vCode.MaxDistance =  3.5
 vCode.PickupDistance = 2.5
 
