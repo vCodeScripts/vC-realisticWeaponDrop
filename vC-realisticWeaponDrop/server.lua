f530a397f13897dbaddfb7bcb199af4c11c0fb35
whit333 <106171903+whit333@users.noreply.github.com>
2022-08-02 17:46:20 +0200

    Update server.lua
    Fixed dupe

--- vC-realisticWeaponDrop/server.lua
+++ vC-realisticWeaponDrop/server.lua
@@ -1,5 +1,6 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
+takeds = {}
 
 RegisterServerEvent('vC-realisticWeaponDrop:server:syncforEveryone', function(name, info, hash, pos)
     TriggerClientEvent('vC-realtisticWeaponDrop:dropweapon', -1, name,info,hash, pos)
@@ -8,11 +9,18 @@ end)
 
 RegisterServerEvent('vC-realsticWeaponDrop:server:removeforEveryone', function(item,info,serie)
     local Player = QBCore.Functions.GetPlayer(source)
-    if Player.Functions.AddItem(item, 1,false,info) then
-        local label = QBCore.Shared.Items[item].label
-        TriggerClientEvent('QBCore:Notify', Player.PlayerData.source, 'You Picked Up A '..label..' With Serial Number '..serie..' !')
-        TriggerClientEvent('vC-realsticWeaponDrop:client:removeforEveryone',-1, serie)
-    else
-        TriggerClientEvent('QBCore:Notify', Player.PlayerData.source, 'You are too full to pick up this weapon!', 'error')
+        if takeds[serie] == nil then
+            if Player.Functions.AddItem(item, 1,false,info) then
+                takeds[serie] = 'taked'
+                local label = QBCore.Shared.Items[item].label
+                TriggerClientEvent('QBCore:Notify', Player.PlayerData.source, 'You Picked Up A '..label..' With Serial Number '..serie..' !')
+                TriggerClientEvent('vC-realsticWeaponDrop:client:removeforEveryone',-1, serie)
+                Citizen.Wait(5000)
+                takeds[serie] = nil
+                return
+            else
+                        takeds[serie] = nil
+                TriggerClientEvent('QBCore:Notify', Player.PlayerData.source, 'You are too full to pick up this weapon!', 'error')
+            end
     end
 end)
